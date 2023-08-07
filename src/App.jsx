5c718c6120b6934543440824482a640748ba4300
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2023-08-07 16:00:39 -0400

    tratando de que este repo copile con toda la informacion de pdf.negra.hipolita , ya que este repo no se puede copilar en netlify, revisar y acomodar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {Button} from "react-bootstrap";
+import VistaWeb from "./VistaWeb";
+import DocuPdf from "./DocuPdf";
+import { PDFViewer,PDFDownloadLink } from "@react-pdf/renderer";
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more ,,, un cambio mas
-      </p>
-    </>
-  )
+
+  const [randomCite, setRandomCite] = useState(null)
+  const Random=Math.floor(numeroAleatorioDecimales())
+  const [verWeb, setVerWeb] = useState(false)
+  const [verPdf, setfVerPdf] = useState(false)
+  
+  const Menu = () => {
+    return(
+
+      <nav>
+       <Button onClick={()=>{
+        setVerWeb(!verWeb);
+        setfVerPdf(false);
+       }}>{verWeb ? " Ocultar Web" : " Ver Web" }</Button>
+
+       <Button onClick={()=>{
+        setfVerPdf(!verPdf);
+        setVerWeb(false)
+       }}>{verPdf ? "Ocultar PDF" : "Ver PDF"}</Button>
+       <PDFDownloadLink document={<DocuPdf texto={randomCite}/>}fileName="Caso.pdf">
+
+       <Button >Descargar PDF</Button>
+       </PDFDownloadLink>
+
+    </nav>
+      );
+  };
+  useEffect(() => {
+  const URL=`https://baconipsum.com/api/?type=all-meat&paras=${Random}&start-with-lorem=1`
+    
+  axios.get(URL)
+  .then(res=>setRandomCite(res.data))
+  .catch(err=>console.log(err))
+}, [])
+console.log(randomCite)
+
+
+function numeroAleatorioDecimales() {
+   var min=0, max=10
+  var num = Math.random() * (max - min);
+  return num + min;
 }
 
-export default App
+  return (
+
+    <div style={{minHeight:"100vh"}}>
+      hola
+      <Menu/>
+      {randomCite ? 
+      <>
+    { verWeb ? <VistaWeb texto={randomCite}/>: null}
+    { verPdf ?(
+      <PDFViewer style={{width:"100%", height:"100vh"}}>
+        <DocuPdf texto={randomCite}/>
+      </PDFViewer>
+     ): null}
+
+      </> :null}
+      
+    
+    
+    
+    </div>
+  );
+}
+
+export default App;
+
